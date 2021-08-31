2a6fda2b5e3f74d138ab637f2df55fc386186bba
Derek <derek.liu4@gmail.com>
2021-08-31 21:34:53 +1000

    Day 6 start

--- chapter1/day6.py
+++ chapter1/day6.py
@@ -1,5 +1,5 @@
 ###1.6.1
-
+#E.g 1
 def sum_naturals(n)
     total, k = 0,1
     while k<=n:
@@ -10,6 +10,7 @@ def sum_naturals(n)
 sum_naturals(100)
 """5050"""
 
+#E.g 2
 def sum_cubes(n):
     total, k = 0,1
     while k<=n:
@@ -21,6 +22,7 @@ sum_cubes(100)
 25502500
 """
 
+#E.g 3
 def pi_sum(n):
     total, l = 0,1
     while k<=n:
@@ -31,6 +33,72 @@ pi_sum(100)
 """
 3.1365926848388144
 """
+
+#Combination example
+def summation(n, term):
+    total, k = 0,1
+    while k <=n:
+        total, k = total + term(k), k + 1
+    return total
+
+def cube(x):
+    return x*x*x
+
+def sum_cubes(n):
+    return summation(n,cube)
+
+result = sum_cubes(3)
+
+
+#Identity combination example
+def summation(n,term):
+    total, k = 0, 1
+    while k<=n:
+        total, k = total + term(k), k + 1
+    return total
+
+def identity(x):
+    return x
+
+def sum_naturals(n):
+    return summation(n, identity)
+
+sum_naturals(10)
+" 55 "
+
+summation(10, square)
+"385"
+
+#Pi example
+def pi_term(x)
+    return 8/ ((4*x-3) * (4*x-1))
+
+def pi_sum(n):
+    return summation(n, pi_term)
+
+pi_sum(1e6)
+"3.141592153589902"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- notes/day6.md
+++ notes/day6.md
@@ -7,3 +7,56 @@
 ###1.6.1
 
 ***Functions as arguments***
+- 
+
+**Summation Example**
+- The example creates the function summation which can be shared
+- The "combination example" works via 2 components
+- First being the summation of the values
+- Second being the special transformation to the values
+
+**Identity Example**
+- The identity function can be used to return its argument
+- This example highlights how additional transformations could be added in
+
+**Direct Example**
+- Function can be used directly without defining another function
+- Prebuilt functions can be utilised 
+
+**Pi Example**
+- Summation can be used as a white label solution 
+
+###1.6.2
+
+***Functions as General Methods***
+- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+- 
