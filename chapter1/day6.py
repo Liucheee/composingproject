d7183014854cc07bb7c7e97184fe301669d7f0e2
Derek <derek.liu4@gmail.com>
2021-08-31 23:10:51 +1000

    Day 6 start

--- chapter1/day6.py
+++ chapter1/day6.py
@@ -121,8 +121,10 @@ phi = improve(golden_update, square_close_to_successor)
 from math import sqrt
 phi = 1/2 + sqrt(5)/2
 def improve_test():
-    approx_phi = improve(golden_update, )
+    approx_phi = improve(golden_update, square_close_to_successor)
+    assert approx_eq(phi, approx_phi), 'phi differs from its approximation'
 
+improve_test()
 
 
 
