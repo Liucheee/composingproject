982f7745d1204ef3bf65447a7466dd7232c1beef
Derek <derek.liu4@gmail.com>
2021-09-28 22:56:49 +1000

    Day 6 midnight sesh

--- chapter1/day7.py
+++ chapter1/day7.py
@@ -1,1 +1,26 @@
 ###1.7
+#Example 1
+>>> 18117 % 10
+7
+
+>>> 18117 // 10
+1811
+
+#Example 2
+Def sum_digits(n):
+    """Return the sum of the digits of positive integer n."""
+    if n < 10:
+        return n
+    else:
+        all_but_last, last = n //10, n % 10
+        return sum_digits(all_but_last) + last
+
+
+
+
+
+
+
+
+
+
--- notes/day7.md
+++ notes/day7.md
@@ -2,3 +2,25 @@
 
 ***Recursive functions***
 - 
+
+
+- Recursive functions are just if the body of the function calls the function itself either directly or indirectly
+- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
